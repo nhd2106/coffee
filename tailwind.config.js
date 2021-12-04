9b741f8a62316ccb87eea852899626aa265a2caa
Hữu Được <nhd.ars@gmail.com>
2021-12-04 22:31:54 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   mode: 'jit',
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
-
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
