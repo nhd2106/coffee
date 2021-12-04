aef1b6073dfd54c1a7fcea1f4f16e7dc05bfa8ca
Hữu Được <nhd.ars@gmail.com>
2021-12-04 22:45:52 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layout/**/*.{js,ts,jsx,tsx}',
-    './helpers/**/*.{js,ts,jsx,tsx}',
-    // Add more here
-],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
