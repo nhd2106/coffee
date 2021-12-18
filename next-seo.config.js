692391c26b92dcc5122221544e6b4a208aaead04
Hữu Được <nhd.ars@gmail.com>
2021-12-18 15:20:17 +0700

    edit seo config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,12 @@
 export default {
     title : "Tùng Coffee",
     titleTemplate : 'Tùng Coffee | %s',
-    canonical: 'https://tungcoffee.vercel.app/',
+    canonical: 'https://www.tungcoffee.com/',
     description: 'Tùng coffee, số 4 đường D9, Tân phú, Hồ Chí Minh. Cà phê ngon quận tân phú, quán cf đẹp quận tân phú',
     openGraph: {
         type: 'website',
         locale: 'vi_VN',
-        url : 'https://tungcoffee.vercel.app/',
+        url : 'https://www.tungcoffee.com/',
         site_name: 'tungcoffee',
         images : [
             {
